0b6939a0e90b543a3cb33a00d19ec3dc904ae4f7
Reza Khan <rezakhan995@gmail.com>
2020-05-03 15:12:31 +0600

    taxonomy control added

--- devmonsta/options/taxonomies/post_tag.php
+++ devmonsta/options/taxonomies/post_tag.php
@@ -2,35 +2,52 @@
 
 use Devmonsta\Libs\Taxonomies;
 
-class Post_Tag extends Taxonomies
-{
+class Post_Tag extends Taxonomies {
 
-    public function register_controls()
-    {
+    public function register_controls() {
 
-        $this->add_control([
-            'type' => 'text',
+        $this->add_control( [
+            'type'        => 'text',
             'placeholder' => 'Tags first name',
-            'label' => 'First name',
-            'name' => 'p_f_name',
-        ]);
+            'label'       => 'Name',
+            'name'        => 'p_f_name',
+        ] );
 
-        $this->add_control([
-            'type' => 'text',
-            'placeholder' => 'Tags last name',
-            'label' => 'Last name',
-            'name' => 'p_l_name',
-        ]);
+        // $this->add_control( [
+        //     'name'        => 'p_l_name',
+        //     'type'        => 'textarea',
+        //     'placeholder' => 'Tags last name',
+        //     'label'       => __( 'Details', '{domain}' ),
+        //     'desc'        => __( 'this is small description', '{domain}' ),
+        // ] );
 
-        $this->add_control([
-            'type' => 'text',
-            'placeholder' => 'Tags first name',
-            'label' => 'Email',
-            'name' => 'p_a_name',
-            'show_in_table' => true
-        ]);
+        /**
+         * control for url input
+         */
+        $this->add_control( [
+            'name'          => 'tax_url',
+            'type'          => 'url',
+            'label'         => __( 'URL', '{domain}' ),
+            'desc'          => __( 'Url Description', '{domain}' ),
+            'show_in_table' => true,
+        ] );
 
-        
+        /**
+         * control for radio input
+         */
+        // $this->add_control( [
+        //     'name'    => 'gender',
+        //     'type'    => 'radio',
+        //     'desc'    => 'use this area to input radio',
+        //     'label'   => __( 'Gender', '{domain}' ),
+        //     'value'   => 'choice-3',
+        //     'choices' => [
+        //         'choice-1' => __( 'Male', '{domain}' ),
+        //         'choice-2' => __( 'Female', '{domain}' ),
+        //         'choice-3' => __( 'Others', '{domain}' ),
+        //     ],
+        //     'inline'  => false,
+        // ] );
 
     }
 
