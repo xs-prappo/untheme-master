8cbdb5d08a74484ac00605cc8eba08bec85d70d2
Prappo Prince <prappo@xpeedstudio.com>
2020-05-30 15:01:26 +0600

    Update post.php

--- devmonsta/options/posts/post.php
+++ devmonsta/options/posts/post.php
@@ -488,43 +488,45 @@ class Post extends Posts
 
         
 
-        // $this->add_control( [
-        //     'box_id'   => 'post_box_2',
-        //     'type'     => 'repeater',
-        //     'label'    => 'My Repeater',
-        //     'controls' => [
-        //         [
-        //             'name'  => 'user_url_1',
-        //             'type'  => 'url',
-        //             'value' => 'http://www.reza-khan.com',
-        //             'label' => __( 'Enter valid URL', '{domain}' ),
-        //             'desc'  => __( 'Url Description', '{domain}' ),
-        //         ],
-        //         [
-        //             'type'    => 'radio',
-        //             'name'    => 'gender_1',
-        //             'desc'    => 'use this area to input radio',
-        //             'label'   => __( 'Gender', '{domain}' ),
-        //             'value'   => 'choice-3',
-        //             'choices' => [
-        //                 // Note: Avoid bool or int keys http://bit.ly/1cQgVzk
-        //                 'choice-1' => __( 'Male', '{domain}' ),
-        //                 'choice-2' => __( 'Female', '{domain}' ),
-        //                 'choice-3' => __( 'Others', '{domain}' ),
-        //             ],
-        //             // Display choices inline instead of list
-        //             'inline'  => true,
-        //         ],
-        //         [
-        //             'name'     => 'color',
-        //             'type'     => 'color-picker',
-        //             'label'    => __( 'Wp Color Picker Example', '{domain}' ),
-        //             'desc'     => __( 'description of color-picker goes here', '{domain}' ),
-        //             'value'    => '#FF0000',
-        //             'palettes' => ['#ba4e4e', '#0ce9ed', '#941940'],
-        //         ],
-        //     ],
-        // ] );
+        $this->add_control( [
+            'box_id'   => 'post_box_2',
+            'type'     => 'repeater',
+            'name'     => 'repeater_1',
+            'label'    => 'My Repeater',
+            'add_new' => 'Add new',
+            'controls' => [
+                [
+                    'name'  => 'user_url_1',
+                    'type'  => 'url',
+                    'value' => 'http://www.reza-khan.com',
+                    'label' => __( 'Enter valid URL', '{domain}' ),
+                    'desc'  => __( 'Url Description', '{domain}' ),
+                ],
+                [
+                    'type'    => 'radio',
+                    'name'    => 'gender_1',
+                    'desc'    => 'use this area to input radio',
+                    'label'   => __( 'Gender', '{domain}' ),
+                    'value'   => 'choice-3',
+                    'choices' => [
+                        // Note: Avoid bool or int keys http://bit.ly/1cQgVzk
+                        'choice-1' => __( 'Male', '{domain}' ),
+                        'choice-2' => __( 'Female', '{domain}' ),
+                        'choice-3' => __( 'Others', '{domain}' ),
+                    ],
+                    // Display choices inline instead of list
+                    'inline'  => true,
+                ],
+                [
+                    'name'     => 'color',
+                    'type'     => 'color-picker',
+                    'label'    => __( 'Wp Color Picker Example', '{domain}' ),
+                    'desc'     => __( 'description of color-picker goes here', '{domain}' ),
+                    'value'    => '#FF0000',
+                    'palettes' => ['#ba4e4e', '#0ce9ed', '#941940'],
+                ],
+            ],
+        ] );
 
 
         // dm_print(dm_p()->option('8', 'color'));
