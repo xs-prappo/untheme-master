31fd42fe92973d6f821cb2d684678bd9c33a8edc
Reza Khan <rezakhan995@gmail.com>
2020-07-06 18:45:29 +0600

    theme demos updated

--- devmonsta/theme-demos.php
+++ devmonsta/theme-demos.php
@@ -8,7 +8,7 @@ function devm_import_files_theme() {
             'import_file_url'          => 'http://demo.themewinter.com/wp/demo-content/privsa/main/privsa-main.xml',
             'import_desc'              => 'Here goes the description of this demo 1 to 2',
             'import_preview_image_url' => 'http://demo.themewinter.com/wp/demo-content/privsa/main/screenshot.png',
-            'required_plugin'          => ["elementor", "elementskit-lite", "metform", "privsa-essential"],
+            'required_plugin'          => ["elementor", "elementskit-lite", "metform"],
         ],
         [
             'id'                       => '2',
@@ -16,9 +16,8 @@ function devm_import_files_theme() {
             'import_file_url'          => 'http://demo.themewinter.com/wp/demo-content/privsa/onepage/onepage.xml',
             'import_desc'              => 'Here goes the description of this demo 1 to 2',
             'import_preview_image_url' => 'http://demo.themewinter.com/wp/demo-content/privsa/onepage/screenshot.png',
-            'required_plugin'          => ["elementor", "elementskit-lite", "metform", "privsa-essential"],
-        ],
+            'required_plugin'          => ["elementor", "elementskit-lite", "metform"],
+        ]
     ];
 }
 add_filter( 'devm_import_demo_files', 'devm_import_files_theme' );
-// devm_print( devm_import_files_theme() );
